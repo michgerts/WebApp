c87a09543529427cdc26fd9a8fa9ed1be35b008f
Rita <rkaufm04@campus.haifa.ac.il>
2016-02-22 00:51:06 +0200

    paging and list of badges - likes

--- WebContent/scripts/NewestQuestions.js
+++ WebContent/scripts/NewestQuestions.js
@@ -1,13 +1,13 @@
 // Front end js
 app.controller('NewestQuestions',function ($scope, $http, $window)
 {
-	$scope.init = function ()//answers a question
+	$scope.init = function ()
 	{
 		$http(
 		{
 			method: 'get',
 			url: 'newestquestionsservlet',
-			headers: {'Content-Type': 'application/json'},
+			headers: {'Content-Type': 'application/json'}
 		}).success( function (response)
 		{				
 			for(var i=0; i<response.length; i++)
@@ -15,8 +15,92 @@ app.controller('NewestQuestions',function ($scope, $http, $window)
 			      var text = response[i].Text;
 			      var time = response[i].Time;
 			      var likes = response[i].Likes;
-			      $("#newQuestionsTable").append('<tr><td>'+ text + '</td><td>'+time+'</td><td>' +'</td><td>'+likes+'</td></tr>');
+			      
+			      
+			      var ul = document.getElementById("newQuestionsList");
+			      var li = document.createElement("li");
+			      li.appendChild(document.createTextNode(text + ' ' + time ));
+			      li.setAttribute("class", "list-group-item");
+			      var span = document.createElement("span");
+			      span.appendChild(document.createTextNode(likes));
+			      span.setAttribute("class", "badge");
+			      li.appendChild(span);
+			      ul.appendChild(li);
+			      //alert(li.class);
+			      
+			     // $("#newQuestionsTable").append('<li>'+ text + '</li><li>'+time+'</li><li>'+likes+'</li>');
+			      //$("#newQuestionsTable").append('<tr><td>'+ text + '</td><td>'+time+'</td><td>' +'</td><td>'+likes+'</td></tr>');
 			} 
 		});
+	}
+	
+	$scope.next = function()
+	{
+		$http(
+				{
+					method: 'get',
+					url: 'newestquestionsservlet',
+					headers: {'Content-Type': 'application/json'},
+					data:  JSON.stringify(1)
+				}).success( function (response)
+				{				
+					var ul = document.getElementById("newQuestionsList");
+			        ul.innerHTML = "";
+					for(var i=0; i<response.length; i++)
+					{
+					      var text = response[i].Text;
+					      var time = response[i].Time;
+					      var likes = response[i].Likes;
+					      
+					      
+					      var li = document.createElement("li");
+					      li.appendChild(document.createTextNode(text + ' ' + time ));
+					      li.setAttribute("class", "list-group-item");
+					      var span = document.createElement("span");
+					      span.appendChild(document.createTextNode(likes));
+					      span.setAttribute("class", "badge");
+					      li.appendChild(span);
+					      ul.appendChild(li);
+					      //alert(li.class);
+					      
+					     // $("#newQuestionsTable").append('<li>'+ text + '</li><li>'+time+'</li><li>'+likes+'</li>');
+					      //$("#newQuestionsTable").append('<tr><td>'+ text + '</td><td>'+time+'</td><td>' +'</td><td>'+likes+'</td></tr>');
+					} 
+				});
+	}
+	
+	$scope.prev = function()
+	{
+		$http(
+				{
+					method: 'get',
+					url: 'newestquestionsservlet',
+					headers: {'Content-Type': 'application/json'},
+					data:  JSON.stringify(0)
+				}).success( function (response)
+				{			
+					var ul = document.getElementById("newQuestionsList");
+					ul.innerHTML = "";	
+					for(var i=0; i<response.length; i++)
+					{
+					      var text = response[i].Text;
+					      var time = response[i].Time;
+					      var likes = response[i].Likes;
+					      
+					      
+					      var li = document.createElement("li");
+					      li.appendChild(document.createTextNode(text + ' ' + time ));
+					      li.setAttribute("class", "list-group-item");
+					      var span = document.createElement("span");
+					      span.appendChild(document.createTextNode(likes));
+					      span.setAttribute("class", "badge");
+					      li.appendChild(span);
+					      ul.appendChild(li);
+					      //alert(li.class);
+					      
+					     // $("#newQuestionsTable").append('<li>'+ text + '</li><li>'+time+'</li><li>'+likes+'</li>');
+					      //$("#newQuestionsTable").append('<tr><td>'+ text + '</td><td>'+time+'</td><td>' +'</td><td>'+likes+'</td></tr>');
+					} 
+				});
 	}
 });
--- src/WebAppPkg/NewestQuestionsServlet.java
+++ src/WebAppPkg/NewestQuestionsServlet.java
@@ -23,8 +23,9 @@ import WebAppPkg.Question;
 public class NewestQuestionsServlet extends HttpServlet
 {//this will submit an answer -- need to change the name
 	private static final long serialVersionUID = 1L;
-	private String dbURL =  "jdbc:derby://localhost:1527/c:/Users/koganmic/Documents/DB/MyDB";
+	private String dbURL =  "jdbc:derby://localhost:1527/C:/Program Files/Derby/db-derby-10.12.1.1-bin/bin/MyDbTest";
     private String tableName = "QUESTIONS";
+    private int page=0;
     @Override
     public void doGet (HttpServletRequest request, HttpServletResponse response)
  		   throws IOException, ServletException
@@ -38,13 +39,32 @@ public class NewestQuestionsServlet extends HttpServlet
     	
             StringBuilder sb = new StringBuilder();
             BufferedReader br = request.getReader();
+        	page = (int) request.getSession().getValue("numOfPage");
             String str = null;
             while ((str = br.readLine()) != null)
             {
                 sb.append(str);
             }
-            questions = db.executeQuery("SELECT * FROM "+ tableName+ " where answered=false order by time desc FETCH FIRST 20 ROWS ONLY ");
+
+    		String nextOrPrev = new Gson().fromJson(sb.toString(),String.class);
     		
+    		if(nextOrPrev != null && !nextOrPrev.isEmpty())
+    		{
+    			if(nextOrPrev.equals(0) && page>0)
+    				request.getSession().putValue("numOfPage", --page);
+        		else
+        			if(nextOrPrev.equals(1))
+        				request.getSession().putValue("numOfPage", ++page);
+    		}
+    		
+    		ResultSet numOfRowsSet = db.executeQuery("select count(*) as A from QUESTIONS");
+    		String numOfRows = "";
+    		if(numOfRowsSet.next())
+    			numOfRows = numOfRowsSet.getString("A");
+    		int numOfRowsInt = Integer.parseInt(numOfRows);//this is for the descending order later
+            questions = db.executeQuery("SELECT * FROM "+ tableName+ " where answered=false order by time asc "+" offset " + page*20 +" rows"+" FETCH FIRST 20 ROWS ONLY ");
+            
+            
             while (questions.next())
     		{
             		Question question = new Question( Integer.parseInt(questions.getString("ID")), questions.getString("Text"),
